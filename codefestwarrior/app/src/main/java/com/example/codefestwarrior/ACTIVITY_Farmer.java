41225759edfb106af5b78831f3d0f8d8a8b02aa8
RSebastian <REdjosephpogi@gmail.com>
2025-04-02 20:34:00 +0800

    sasa

--- codefestwarrior/app/src/main/java/com/example/codefestwarrior/ACTIVITY_Farmer.java
+++ codefestwarrior/app/src/main/java/com/example/codefestwarrior/ACTIVITY_Farmer.java
@@ -1,14 +1,36 @@
 package com.example.codefestwarrior;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewbinding.ViewBinding;
 
 import android.os.Bundle;
+import android.widget.SimpleAdapter;
+
+import com.example.codefestwarrior.databinding.ActivityFarmerBinding;
 
 public class ACTIVITY_Farmer extends AppCompatActivity {
+
+    ActivityFarmerBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_farmer);
+
+        binding = ActivityFarmerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            return true;
+        });
+
+    }
+    void setFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
