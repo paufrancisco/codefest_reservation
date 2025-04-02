02007aeabed8f2c1573d71df34ec3f32d4f47bf5
RSebastian <REdjosephpogi@gmail.com>
2025-04-02 20:26:05 +0800

    pre-frags

--- codefestwarrior/app/src/main/java/com/example/codefestwarrior/MainActivity.java
+++ codefestwarrior/app/src/main/java/com/example/codefestwarrior/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.codefestwarrior;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_FarmerLogin, btn_CustomerLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // INSTANTIATIONS
+        btn_FarmerLogin = findViewById(R.id.btn_FarmerLogin);
+        btn_CustomerLogin = findViewById(R.id.btn_CustomerLogin);
+
+        // FARMER BUTTON HANDLER
+        btn_FarmerLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ACTIVITY_Farmer.class);
+                startActivity(intent);
+            }
+        });
+        // CUSTOMER BUTTON HANDLER
+        btn_CustomerLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ACTIVITY_Customer.class);
+                startActivity(intent);
+            }
+        });
     }
 }
